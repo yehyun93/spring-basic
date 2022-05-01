f72e8a3dc30b6a532330b1e56a43a916e98ac1a9
master <yehyun9321@gmail.com>
2022-05-02 00:19:49 +0900

    Spring Data JPA

--- src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -25,7 +25,6 @@ class MemberServiceIntegrationTest {
     @Autowired MemberRepository memberRepository;
 
     @Test
-    @Commit
     void 회원가입() {
         //given
         Member member = new Member();
